0e73a7e27fed420bb28fdbd3e893721001527323
Mikael Ravantti <Mikael.Ravantti@gmail.com>
2019-01-07 13:02:10 +0100

    Added login function

--- app/users/login.php
+++ app/users/login.php
@@ -3,3 +3,30 @@
 declare(strict_types=1);
 
 require __DIR__.'/../autoload.php';
+
+
+if (isset($_POST['email'], $_POST['password'])) {
+  $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+  $password = filter_var($_POST['password']);
+
+  $stmt = $pdo->prepare('SELECT * FROM users WHERE email = :email');
+  $stmt->bindParam(':email', $email);
+  $stmt->execute();
+  $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if (!$user) {
+  redirect('/login.php');
+
+}
+  if ($email === '' || $password === '') {
+
+  }
+
+  if (password_verify($_POST['password'], $user['password']))
+{
+  $_SESSION['user'] = $user;
+  redirect('/');
+} else {
+  redirect('/login.php');
+}
+}

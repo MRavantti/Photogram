bc8c2a8c6940799622e4ddadc3e2d0addd9c67f7
Mikael Ravantti <Mikael.Ravantti@gmail.com>
2019-01-07 13:09:05 +0100

    ed session message if user is logged in

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php require __DIR__."/views/header.php"; ?>
 
+<h1><?php echo $config['title']; ?></h1>
+    <p>This is the home page.</p>
+    <?php if (isset($_SESSION['user'])): ?>
+        <p>Welcome, <?php echo $_SESSION['user']['first_name']; ?>!</p>
+    <?php endif; ?>
 <h1><a href="/register.php">Register!</a> </h1>
 	<h1><a href="/login.php">Login!</a></h1>
+	<?php if (isset($_SESSION["user"])): ?>
+		<h1><a href="/app/users/logout.php">Logout!</a></h1>
+	<?php endif; ?>
 
 
 
